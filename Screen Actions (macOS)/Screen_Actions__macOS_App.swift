2fe488ec67e407aaa1ec5b8f9db684021d20a76e
Conor <hodlsimulator@gmail.com>
2025-09-24 05:10:02 +0100

    No errors, working on extension again.

---
+++ Screen Actions (macOS)/Screen_Actions__macOS_App.swift
@@ -0,0 +1,17 @@
+//
+//  Screen_Actions__macOS_App.swift
+//  Screen Actions (macOS)
+//
+//  Created by . . on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Screen_Actions__macOS_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
